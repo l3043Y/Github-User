262567a41eb05c9a73c899146eabe1d2e502878a
l3043Y <boreyc2013@gmail.com>
2021-08-21 10:53:45 +0700

    Add githubAPI

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,38 +1,26 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import User from './User';
-import { IGitHubUser } from '../utils/IGithubUser';
-import {preprocessUser} from '../utils/UserPreprocess';
+import User, { IUser } from './User';
+import fetchUsers from '../utils/GitHubAPI';
 
 export default function UserList(): React.ReactElement{
     const [userList, setUserList] = 
-        useState<IGitHubUser[]>( [] as IGitHubUser[] );
-
+        useState<IUser[]>( [] as IUser[] );
 
     useEffect(() => {
-        async function fetchData(){
-            const apiurl: string = process.env.REACT_APP_CONSUME_API as string
-            const axiosRespone = await axios.get<IGitHubUser[]>(apiurl);
-            setUserList(axiosRespone.data.slice(0,20))
-        }
-        fetchData()
+       fetchUsers().then((users:IUser[]) =>{
+           setUserList(users)
+       })
     },[])
-
-    useEffect(()=>{
-        // console.log("debug: ",userList)
-        // console.log("debug len: ",userList.length)
-        console.log('IUser: ', preprocessUser(userList))
-    },[userList])
 
     return (
         <div>
-            {userList.map((user:IGitHubUser) => (
+            {userList.map((user:IUser) => (
                 <User
                     key = {user.id}
-                    userAvatarUrl = {user.avatar_url}
-                    userName = {user.login}
-                    numberFollowing = {5}
-                    NumberFollower = {100}
+                    userAvatarUrl = {user.userAvatarUrl}
+                    userName = {user.userName}
+                    numberFollowing = {user.numberFollowing}
+                    NumberFollower = {user.NumberFollower}
                 />
             ))}
         </div>
